23ce5b0e5d29f3d3386d67af2674490932f24cf0
dreamanlan <dreaman_163@163.com>
2016-05-27 00:20:25 +0800

    技能底层支持字符串特性（用于支持可视化编辑）

--- Infrastructure/SkillSystem/ISkillTriger.cs
+++ Infrastructure/SkillSystem/ISkillTriger.cs
@@ -4,7 +4,99 @@ using System.Text;
 
 namespace SkillSystem
 {
-    public interface ISkillTriger
+    public delegate void VisitPropertyDelegation(string group, string key, IProperty property);
+    public interface IPropertyVisitor
+    {
+        void VisitProperties(VisitPropertyDelegation callback);
+    }
+    public interface IProperty
+    {
+        object Value { get; set; }
+    }
+    public interface IPropertyAccessor
+    {
+        bool TryGetProperty(string key, out object val);//获取变量值
+        void SetProperty(string key, object val);//设置变量值
+    }
+    public sealed class PropertyAccessorHelper : IPropertyVisitor, IPropertyAccessor
+    {
+        private class PropertyWrap : IProperty
+        {
+            public object Value
+            {
+                get
+                {
+                    object val = null;
+                    if (null != OnGet) {
+                        val = OnGet();
+                    }
+                    return val;
+                }
+                set
+                {
+                    if (null != OnSet) {
+                        OnSet(value);
+                    }
+                }
+            }
+
+            internal GetDelegation OnGet;
+            internal SetDelegation OnSet;
+        }
+        public delegate object GetDelegation();
+        public delegate void SetDelegation(object val);
+
+        public void VisitProperties(VisitPropertyDelegation callback)
+        {
+            try {
+                foreach (var pair in m_Properties) {
+                    callback(m_Group, pair.Key, pair.Value);
+                }
+            } catch (Exception ex) {
+                GameFramework.LogSystem.Error("VisitVariables throw exception:{0}\n{1}", ex.Message, ex.StackTrace);
+            }
+        }
+        public bool TryGetProperty(string key, out object val)
+        {
+            try {
+                PropertyWrap wrap;
+                if (m_Properties.TryGetValue(key, out wrap)) {
+                    val = wrap.OnGet();
+                    return true;
+                } else {
+                    val = null;
+                    return false;
+                }
+            } catch (Exception ex) {
+                GameFramework.LogSystem.Error("TryGetVariable {0} throw exception:{1}\n{2}", key, ex.Message, ex.StackTrace);
+                val = null;
+                return false;
+            }
+        }
+        public void SetProperty(string key, object val)
+        {
+            try {
+                PropertyWrap wrap;
+                if (m_Properties.TryGetValue(key, out wrap)) {
+                    wrap.OnSet(val);
+                }
+            } catch (Exception ex) {
+                GameFramework.LogSystem.Error("SetVariable {0} {1} throw exception:{2}\n{3}", key, null == val ? "null" : val.ToString(), ex.Message, ex.StackTrace);
+            }
+        }
+        public void SetGroup(string group)
+        {
+            m_Group = group;
+        }
+        public void AddProperty(string key, GetDelegation onGet, SetDelegation onSet)
+        {
+            m_Properties.Add(key, new PropertyWrap { OnGet = onGet, OnSet = onSet });
+        }
+
+        private string m_Group = string.Empty;
+        private Dictionary<string, PropertyWrap> m_Properties = new Dictionary<string, PropertyWrap>();
+    }
+    public interface ISkillTriger : IPropertyVisitor, IPropertyAccessor
     {
         ISkillTriger Clone();//克隆触发器，触发器只会从DSL实例一次，之后都通过克隆产生新实例
         long GetStartTime();
@@ -15,9 +107,21 @@ namespace SkillSystem
     }
     public abstract class AbstractSkillTriger : ISkillTriger
     {
-        public virtual long GetStartTime()
+        public long GetStartTime()
         {
             return m_StartTime;
+        }
+        public void VisitProperties(VisitPropertyDelegation callback)
+        {
+            m_AccessorHelper.VisitProperties(callback);
+        }
+        public bool TryGetProperty(string key, out object val)
+        {
+            return m_AccessorHelper.TryGetProperty(key, out val);
+        }
+        public void SetProperty(string key, object val)
+        {
+            m_AccessorHelper.SetProperty(key, val);
         }
 
         public void Init(Dsl.ISyntaxComponent config, int dslSkillId)
@@ -55,7 +159,19 @@ namespace SkillSystem
         {
         }
 
+        //下面2个方法必须在子类构造函数里调用，不要在别的地方调用！
+        protected void SetGroup(string group)
+        {
+            m_AccessorHelper.SetGroup(group);
+        }
+        protected void AddProperty(string key, PropertyAccessorHelper.GetDelegation onGet, PropertyAccessorHelper.SetDelegation onSet)
+        {
+            m_AccessorHelper.AddProperty(key, onGet, onSet);
+        }
+
         protected long m_StartTime = 0;
+
+        private PropertyAccessorHelper m_AccessorHelper = new PropertyAccessorHelper();
     }
     public class DummyTriger : AbstractSkillTriger
     {
