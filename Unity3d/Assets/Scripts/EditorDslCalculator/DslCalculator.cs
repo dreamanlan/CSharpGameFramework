e54db46eba6a93a35e7abae8778f16bbff166423
dreamanlan <dreaman_163@163.com>
2020-09-15 22:21:03 +0800

    update dsl

--- Unity3d/Assets/Scripts/EditorDslCalculator/DslCalculator.cs
+++ Unity3d/Assets/Scripts/EditorDslCalculator/DslCalculator.cs
@@ -393,11 +393,13 @@ namespace DslExpression
                     }
                 }
             }
-            else if (idType == Dsl.ValueData.BOOL_TOKEN) {
-                m_Val = id == "true";
-            }
             else {
-                m_Val = id;
+                if (id == "true")
+                    m_Val = true;
+                else if (id == "false")
+                    m_Val = false;
+                else
+                    m_Val = id;
             }
             return true;
         }
@@ -6828,6 +6830,11 @@ namespace DslExpression
                             Debug.LogWarningFormat("DslCalculator error, {0} line {1}", comp.ToScriptString(false), comp.GetLine());
                         }
                         return p;
+                    }
+                    else if(id == "true" || id == "false") {
+                        ConstGet constExp = new ConstGet();
+                        constExp.Load(comp, this);
+                        return constExp;
                     }
                     else {
                         NamedVarGet varExp = new NamedVarGet();
