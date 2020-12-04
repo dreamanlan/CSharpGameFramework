141915d89d1180bd9a511df6d59d38bd8039da12
dreamanlan <dreaman_163@163.com>
2020-12-04 20:46:07 +0800

    删除资源处理工具，单独提一个示例工程

--- Unity3d/Assets/Scripts/Editor/ScriptableDsl/ResourceProcess/MemoryProfiler/Database/Operation/IGroupedColumn.cs
+++
@@ -1,7 +0,0 @@
-namespace Unity.MemoryProfilerForExtension.Editor.Database.Operation
-{
-    internal interface IGroupedColumn
-    {
-        void Initialize(GroupedTable table, Column column, int columnIndex, Grouping.IMergeAlgorithm algo, bool isGroup);
-    }
-}
