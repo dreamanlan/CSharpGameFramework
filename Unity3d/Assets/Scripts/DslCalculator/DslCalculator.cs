7736c4c29fe267aa7b9e5b658e0d80edf983ac47
dreamanlan <dreamanlan@tencent.com>
2023-10-10 16:01:17 +0800

    Update DslCalculator.cs

--- Unity3d/Assets/Scripts/DslCalculator/DslCalculator.cs
+++ Unity3d/Assets/Scripts/DslCalculator/DslCalculator.cs
@@ -8814,9 +8814,11 @@ namespace DslExpression
         public void LoadDsl(IList<Dsl.ISyntaxComponent> statements, List<IExpression> exps)
         {
             foreach (Dsl.ISyntaxComponent comp in statements) {
-                var exp = Load(comp);
-                if (null != exp) {
-                    exps.Add(exp);
+                if (comp.IsValid()) {
+                    var exp = Load(comp);
+                    if (null != exp) {
+                        exps.Add(exp);
+                    }
                 }
             }
         }
