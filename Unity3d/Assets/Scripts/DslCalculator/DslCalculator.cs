49bf8026fafffbae87ac7ab576bae13e294c972a
dreamanlan <dreamanlan@tencent.com>
2023-10-19 11:18:05 +0800

    Update DslCalculator.cs

--- Unity3d/Assets/Scripts/DslCalculator/DslCalculator.cs
+++ Unity3d/Assets/Scripts/DslCalculator/DslCalculator.cs
@@ -8929,7 +8929,7 @@ namespace DslExpression
             RecycleCalculatorValueList(args);
             return r;
         }
-        public CalculatorValue Calc(string func, List<CalculatorValue> args)
+        public CalculatorValue Calc(string func, IList<CalculatorValue> args)
         {
             CalculatorValue ret = 0;
             FuncInfo funcInfo;
@@ -8991,7 +8991,7 @@ namespace DslExpression
             }
             return ret;
         }
-        private CalculatorValue Calc<T>(List<CalculatorValue> args, T funcContext, FuncInfo funcInfo) where T : class
+        private CalculatorValue Calc<T>(IList<CalculatorValue> args, T funcContext, FuncInfo funcInfo) where T : class
         {
             LocalStackPush(args, funcContext, funcInfo);
             try {
@@ -9401,7 +9401,7 @@ namespace DslExpression
             }
         }
 
-        private void LocalStackPush<T>(List<CalculatorValue> args, T funcContext, FuncInfo funcInfo) where T : class
+        private void LocalStackPush<T>(IList<CalculatorValue> args, T funcContext, FuncInfo funcInfo) where T : class
         {
             var si = StackInfo.New();
             if (null != args) {
